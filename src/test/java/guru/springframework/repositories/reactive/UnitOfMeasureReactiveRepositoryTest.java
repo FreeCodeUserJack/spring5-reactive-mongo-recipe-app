ef8ce3f71e4f032332479bab17ef24db2042792b
nate <clevelandsun@gmail.com>
2020-06-01 21:00:14 -0400

    service layer prep

---
+++ src/test/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepositoryTest.java
@@ -0,0 +1,36 @@
+package guru.springframework.repositories.reactive;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataMongoTest
+public class UnitOfMeasureReactiveRepositoryTest {
+
+    @Autowired
+    UnitOfMeasureReactiveRepository unitOfMeasureReactiveRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        unitOfMeasureReactiveRepository.deleteAll().block();
+    }
+
+    @Test
+    public void testSave() {
+        UnitOfMeasure unitOfMeasure = new UnitOfMeasure();
+        unitOfMeasure.setDescription("desc");
+
+        unitOfMeasureReactiveRepository.save(unitOfMeasure).block();
+
+        Long count = unitOfMeasureReactiveRepository.count().block();
+
+        assertEquals(Long.valueOf(1L), count);
+    }
+}
